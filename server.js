03f4e8e0ed7e3e3294627a326c16ffa9490c782a
Abhishek Datta <abdatta@iitk.ac.in>
2019-09-11 00:28:06 +0530

    Add self pinging to prevent idling in heroku

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const app = require('express')();
 app.get('/', (req, res) => res.send('Hi! I am soumit-bot. Find me on github and discord.'));
 
 app.listen(process.env.PORT || 4869);
+
+if (process.env.PING_HOST && process.env.PING_INTERVAL) {
+    const request = require('request');
+    const ping = () => request(process.env.PING_HOST, (error, response, body) => {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print body of response received
+    });
+    ping();
+    setInterval(ping, process.env.PING_INTERVAL);
+}
